6c9c5e23ed66dd63260750adce162937628e9b9c
Arshya Soleimani <arrshya@gmail.com>
2023-06-28 19:18:50 +0000

    added seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
+
+
+INSERT INTO users (id, name, email password)
+VALUES (1,'Eva Stanley','sebastianguerra@ymail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(2,'Louisa Meyer','jacksonrose@hotmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(3,'Dominic Parks','victoriablackwell@outlook.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces,
+number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+-- VALUES()
+
+-- Not going to do the rest as we will be using given data anyway.
+
+
